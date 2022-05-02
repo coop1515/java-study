d6f801f7d974fc5fba3260868b71b43e46f1ea64
김현석 <gustjr324@gmail.com>
2022-05-02 18:23:05 +0900

    finaltest

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -2,21 +2,33 @@ package prob5;
 
 public class MyStack {
 	private int top = 0;
+	private String rename ;
 	private String[] buffer;
-	
+
 	public MyStack(int size) {
 		buffer = new String[size];
 	}
-	
+
 	public void push(String item) {
-		this.buffer[0] = item;
+		
+		this.buffer[top] = item;
+		top++;
 	}
-	
-	public String pop() {
-		return null;
+
+	public String pop() throws MyStackException {
+		
+//		for (int i = buffer.length; i > 0; i--) {
+//			rename = buffer[i];
+//		}
+//		if (false) {
+//			throw new MyStackException();
+//		}
+		return rename;
 	}
-	
+
 	public boolean isEmpty() {
+		//버퍼안에 공간이 있는지 확인하는 친구
+		System.out.println(buffer);
 		return false;
 	}
 }
--- practice05/src/main/java/prob5/MyStackException.java
+++ practice05/src/main/java/prob5/MyStackException.java
@@ -2,7 +2,11 @@ package prob5;
 
 public class MyStackException extends Exception {
 	
-	public MyStackException(String message) {
-//		System.out.println("Prob5.MyStackException: stack is empty");
+	public MyStackException() {
+		System.out.println("error");
+	}
+	
+	public MyStackException(String e) {
+		System.out.println("er");
 	}
 }
