28cb57eb77c90e248a3232aa23e3ef4b78c4c4bc
김현석 <gustjr324@gmail.com>
2022-05-03 10:14:23 +0900

    Generic Method & 가변 파라미터

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,22 +2,37 @@ package prob01;
 
 public class Printer {
 
-	public void println(int integer) {
-		System.out.println(integer);
-		
-	}
-	public void println(boolean trueFalse) {
-		System.out.println(trueFalse);
-		
-	}
-	public void println(double real) {
-		System.out.println(real);
-		
+//	public void println(int integer) {
+//		System.out.println(integer);
+//		
+//	}
+//	public void println(boolean trueFalse) {
+//		System.out.println(trueFalse);
+//		
+//	}
+//	public void println(double real) {
+//		System.out.println(real);
+//		
+//	}
+//	
+//	public void println(String korean) {
+//		System.out.println(korean);
+//		
+//	}
+//	public <T, S> void println(T t, S s) { //여러개도 가능한데 너무 많으면 복잡해짐.
+//		System.out.println(t);
+//	}
+	public <T> void println(T... ts) { //여러개도 가능한데 너무 많으면 복잡해짐.
+		for(T t : ts) {
+			System.out.println(t);
+		}
 	}
 	
-	public void println(String korean) {
-		System.out.println(korean);
-		
+	public int sum(int... nums) { // int... 변수가 여러개라는 뜻. (가변)
+		int sum = 0;
+		for(int n : nums) {
+			sum += n;
+		}
+		return sum;
 	}
-		
 }
