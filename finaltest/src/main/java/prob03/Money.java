0259c3451fabd61e7ffa3824d7d7ed7e224c11e5
김현석 <gustjr324@gmail.com>
2022-05-04 17:57:31 +0900

    finaltest04

--- finaltest/src/main/java/prob03/Money.java
+++ finaltest/src/main/java/prob03/Money.java
@@ -2,6 +2,33 @@ package prob03;
 
 public class Money {
 	private int amount;
-	
 	/* 코드 작성 */
+	public Money(int i) {
+		this.amount = i;
+	}
+
+	public Object add(Money money) {
+//		this.amount = amount + 
+		return amount;
+	}
+
+	public Object minus(Money money) {
+
+		return null;
+	}
+
+	public Object multiply(Money money) {
+
+		return null;
+	}
+
+	public Object devide(Money money) {
+
+		return null;
+	}
+	
+	public boolean equals(Object obj) {
+		return true;
+	}
+	
 }
--- finaltest/src/main/java/prob04/Person.java
+++ finaltest/src/main/java/prob04/Person.java
@@ -1,9 +1,31 @@
 package prob04;
 
 public class Person {
-	private static int numberOfPerson; // 전체 인구수
-	private int age;
-	private String name;
 	
+	private static int numberOfPerson; // 전체 인구수
+	private int age = 12;
+	private String name = "";
 	/* 코드 작성 */
+	public Person() {
+			}
+	public Person(String string) {
+		this.name = string;
+		this.numberOfPerson++;
+	}
+	public Person(int i, String string) {
+		this.age = i;
+		this.name = string;
+		this.numberOfPerson++;
+	}
+	public static String getPopulation() {
+		String str = String.valueOf(numberOfPerson);
+		return str;
+	}
+	
+	public void selfIntroduce() {
+		System.out.println("내 이름은 "+name+"이며, 나이는 "+age+" 입니다.");
+		
+	}
+	
+	
 }
