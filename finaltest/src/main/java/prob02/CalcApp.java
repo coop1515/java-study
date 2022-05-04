12000912211f0c73ce6e737d36824b31f923169f
김현석 <gustjr324@gmail.com>
2022-05-04 17:33:36 +0900

    finaltest02

--- finaltest/src/main/java/prob02/CalcApp.java
+++ finaltest/src/main/java/prob02/CalcApp.java
@@ -7,34 +7,42 @@ public class CalcApp {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		while( true ) {
-			System.out.print( ">> " );
+		while (true) {
+			System.out.print(">> ");
 			String expression = scanner.nextLine();
-			
-			if( "quit".equals( expression ) ) {
+
+			if ("quit".equals(expression)) {
 				break;
 			}
-			
-			String[] tokens = expression.split( " " );
-			
-			if( tokens.length != 3 ) {
-				System.out.println( ">> 알 수 없는 식입니다.");
+
+			String[] tokens = expression.split(" ");
+
+			if (tokens.length != 3) {
+				System.out.println(">> 알 수 없는 식입니다.");
 				continue;
 			}
-			
-			int lValue = Integer.parseInt( tokens[ 0 ] );
-			int rValue = Integer.parseInt( tokens[ 1 ] );
-			
+
+			int lValue = Integer.parseInt(tokens[0]);
+			int rValue = Integer.parseInt(tokens[1]);
+
 			Arithmetic arithmetic = null;
-			
 			/* 코드 작성 */
-//			String cal = tokens[3];
-			
-			
+			char cal = tokens[2].charAt(0);
+
+			if (cal == '+') {
+				arithmetic = new Add();
+			} else if (cal == '-') {
+				arithmetic = new Sub();
+			} else if (cal == '*') {
+				arithmetic = new Mul();
+			} else if (cal == '/') {
+				arithmetic = new Div();
+			}
+
 			int result = arithmetic.calculate(lValue, rValue);
-			System.out.println( ">> " + result );
+			System.out.println(">> " + result);
 		}
-		
+
 		scanner.close();
 	}
 }
