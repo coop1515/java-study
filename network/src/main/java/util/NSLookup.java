1c1f049f0bcebdea06880df7d18732c015d257d5
김현석 <gustjr324@gmail.com>
2022-05-06 18:35:45 +0900

    NSLookup

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -1,9 +1,31 @@
 package util;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class NSLookup {
 
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+
+		try {
+			while (true) {
+				System.out.print(">");
+				String line = scanner.nextLine();
+				
+				if (line.equals("exit")) {
+					return;
+				}
+				InetAddress[] inetAddress = InetAddress.getAllByName(line);
+				for (int i = 0; i < inetAddress.length; i++) {
+					System.out.println(line + " : " + inetAddress[i].getHostAddress());
+				}
+			}
+		} catch (UnknownHostException e) {
+			System.out.println("잘못된 도메인 입니다.");
+		}
+
 	}
 
 }
